3b9ca2b8760e732a3b91520bea6579825193ccca
CzimerM <airamek@teamorange.hu>
2023-10-01 22:41:56 +0200

    Add project files.

---
+++ szgya-employee/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szgya_employee
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public bool Gender { get; set; }
+        public bool MaritalStatus { get; set; }
+        public int Salary { get; set; } //EUR
+
+        public int SalaryYearlyHUF => this.SalaryYearly * 390;
+        public int SalaryYearly => this.Salary * 12;
+        public Employee(string line)
+        {
+            string[] data = line.Split(';');
+            this.Name = data[0];
+            this.Age = int.Parse(data[1]);
+            this.City = data[2];
+            this.Department = data[3];
+            this.Position = data[4];
+            this.Gender = data[5] == "Male";
+            this.MaritalStatus = data[6] == "Married";
+            this.Salary = int.Parse(data[7]);
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{this.Name,-9}|{this.Age,-2}|{this.City,-10}|{this.Department,-15}|{this.Position,-15}|{(this.Gender ? "Male" : "Female"),-6}|{(this.MaritalStatus ? "Married" : "Single"),-7}|{this.Salary,-5}";
+        }
+    }
+}
---
+++ szgya-employee/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace szgya_employee
+{
+    class Program
+    {
+        static Dictionary<string, int> Above12MilHUFYearly(List<Employee> employees)
+        {
+            return employees
+                .Where(e => e.SalaryYearlyHUF > 12000000)
+                .ToDictionary(k => k.Name, v => v.SalaryYearlyHUF);
+        }
+
+        static bool YoungestAndOldest(List<Employee> employees, Employee youngest, Employee eldest)
+        {
+            youngest = employees.MinBy(e => e.Age);
+            eldest = employees.MaxBy(e => e.Age);
+            return employees.FindAll(e => e.Age == employees.Min(e => e.Age)).Count > 1;
+        }
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            using StreamReader sr = new StreamReader("../../../employees.txt");
+            while (!sr.EndOfStream)
+            {
+                employees.Add(new Employee(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"8.feladat: átlag életkor: {employees.Average(e => e.Age)}");
+            Console.WriteLine($"9.feladat: budapesti lakosok száma: {employees.Count(e => e.City == "Budapest")}");
+            Console.WriteLine($"10.feladat: legidősebb személy: \n{employees.MaxBy(e => e.Age)}");
+            Console.WriteLine($"11.feladat: {(employees.Exists(e => e.Age > 50) ? "van" : "nincs")}");
+            Console.WriteLine("12.feladat");
+            var f12arr = employees.Where(e => e.Age < 30).ToArray();
+            foreach (var employee in f12arr)
+            {
+                Console.WriteLine(employee);
+            }
+
+            // 15
+            using StreamWriter sw = new StreamWriter("../../../12mil.txt");
+            foreach (var item in Above12MilHUFYearly(employees))
+            {
+                sw.WriteLine($"{item.Key};{item.Value}");
+            }
+
+            Console.WriteLine($"16.feladat: átlag fizetés: {employees.Average(e => e.Salary)}");
+
+            Console.WriteLine($"17.feladat: dev átlagfizetés: {employees.Where(e => e.Position == "Developer").Average(e => e.Salary)} EUR");
+
+            Console.WriteLine($"18.feladat: \n\tférfi átlagfizetés:{employees.Where(e => e.Gender).Average(e => e.Salary)}");
+
+        }
+    }
+}
